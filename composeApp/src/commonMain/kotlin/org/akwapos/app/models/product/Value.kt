543d4a9942b294f9fcd980669800b04dbcd36dbb
aigbo <aigbojeohiorenua@gmail.com>
2025-08-29 16:43:10 +0100

    impl pos products

---
+++ composeApp/src/commonMain/kotlin/org/akwapos/app/models/product/Value.kt
@@ -0,0 +1,29 @@
+package org.akwapos.app.models.product
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Value(
+    @SerialName("attribute")
+    val attribute: Double?,
+    @SerialName("attribute_name")
+    val attributeName: String?,
+    @SerialName("attribute_type")
+    val attributeType: String?,
+    @SerialName("color_code")
+    val colorCode: String?,
+    @SerialName("display_value")
+    val displayValue: String?,
+    @SerialName("effective_display_value")
+    val effectiveDisplayValue: String?,
+    @SerialName("id")
+    val id: Int?,
+    @SerialName("is_active")
+    val isActive: Boolean?,
+    @SerialName("sort_order")
+    val sortOrder: Int?,
+    @SerialName("value")
+    val value: String?
+)
