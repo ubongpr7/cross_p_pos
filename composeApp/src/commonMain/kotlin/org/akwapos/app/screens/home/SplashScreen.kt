73067d5582271aa4e27ad45faf71bfc187da188d
aigbo <aigbojeohiorenua@gmail.com>
2025-09-04 14:03:25 +0100

    logo added

--- composeApp/src/commonMain/kotlin/org/akwapos/app/screens/home/SplashScreen.kt
+++ composeApp/src/commonMain/kotlin/org/akwapos/app/screens/home/SplashScreen.kt
@@ -1,6 +1,8 @@
 package org.akwapos.app.screens.home
 
 
+import akwapos.composeapp.generated.resources.Res
+import akwapos.composeapp.generated.resources.logo_wdout_bg
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
@@ -10,16 +12,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import cafe.adriel.voyager.core.screen.Screen
-import compose.icons.TablerIcons
-import compose.icons.tablericons.ShoppingCart
 import org.akwapos.app.theme.PixelDensity
+import org.jetbrains.compose.resources.painterResource
 
 object SplashScreen: Screen {
     @Composable
     override fun Content() {
         Box(Modifier.fillMaxSize(), Alignment.Center){
             Icon(
-                TablerIcons.ShoppingCart,
+                painter = painterResource(Res.drawable.logo_wdout_bg),
                 "splash icon",
                 tint=Color.Green.copy(alpha = 0.5f),
                 modifier = Modifier.size(PixelDensity.large*10)
